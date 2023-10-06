2f98a90791e5e26e6316ae905f028feba96eaf5a
rvsiyad <roblesiyad123@gmail.com>
2023-10-06 17:05:24 +0100

    Created getSingleMovie method to return movies by Imdb Id

--- src/main/java/dev/rvsiyad/movies/MovieController.java
+++ src/main/java/dev/rvsiyad/movies/MovieController.java
@@ -1,11 +1,13 @@
 package dev.rvsiyad.movies;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,11 @@ public class MovieController {
   public ResponseEntity<List<Movie>> getAllMovies() {
     return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
   }
+ 
+  @GetMapping("/{imdbId}")
+  public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId) {
+    return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId), HttpStatus.OK);
+  }
+
   
 }
