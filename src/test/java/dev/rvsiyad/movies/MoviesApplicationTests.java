a17d2a3d1b6e4708b013f244d812171958f8c84f
rvsiyad <roblesiyad123@gmail.com>
2023-10-06 12:07:54 +0100

    Initial commit

---
+++ src/test/java/dev/rvsiyad/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rvsiyad.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
